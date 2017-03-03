d07d42f7c667a277852c7f0c4636d243aa140c8f
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2017-03-03 10:22:46 +0100

    Clean code

--- linux-ble.sh
+++ linux-ble.sh
@@ -3,10 +3,19 @@
 # Description:		Ble SNR 
 # Author:			Aravinth Panchadcharam
 
+ble_scan_log="logs/ble-scan.log"
+ble_notify_log="logs/ble-notify.log"
+
+# Run lescan every time because gatttool says "No route to host", if devices are not scanned
+hcitool lescan &> $ble_scan_log &
+
+
 # kill $(ps aux | grep 'gatttool' | grep -v grep | awk '{print $2}')
 # sleep 3
 
-gatttool -b F9:8D:BB:F1:15:34 -t random --char-write-req -a 0x002c -n 01 --listen &> ble-snr.log &
+
+
+# gatttool -b F9:8D:BB:F1:15:34 -t random --char-write-req -a 0x002c -n 01 --listen &> ble-snr.log &
 # sleep 3 
 
 # res="$(cat ble-snr.log | grep "No route to host" | wc -l)"
@@ -14,15 +23,15 @@ gatttool -b F9:8D:BB:F1:15:34 -t random --char-write-req -a 0x002c -n 01 --liste
 
 # if [ $res -gt 0 ]; then
 # 	echo "Device not found; lescan"
-# 	hcitool lescan &> a.log &
+# 	
 # 	sleep 5
 # 	kill $(ps aux | grep 'hcitool' | grep -v grep | awk '{print $2}')
 # 	sleep 3
 # 	gatttool -b F9:8D:BB:F1:15:34 -t random --char-write-req -a 0x002c -n 01 --listen &> ble-snr.log &
 # fi
 
-sleep 10
-kill $(ps aux | grep 'gatttool' | grep -v grep | awk '{print $2}') > /dev/null 2>&1
-sleep 3
-res="$(cat ble-snr.log | grep Notification | wc -l)"
-echo $res
+# sleep 10
+# kill $(ps aux | grep 'gatttool' | grep -v grep | awk '{print $2}') > /dev/null 2>&1
+# sleep 3
+# res="$(cat ble-snr.log | grep Notification | wc -l)"
+# echo $res
