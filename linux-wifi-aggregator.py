b9e95d01cf6c2dbde3b79957906e046a24e58333
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2017-03-10 17:09:11 +0100

    Update RSSI aggregator script

--- linux-wifi-aggregator.py
+++ linux-wifi-aggregator.py
@@ -6,69 +6,79 @@
 import csv
 import datetime as dt
 import time
+import sys
 
-log_file_name = 'logs/ampak-ap6212-wifi-rssi-03-03-2017'
-log_file = open(log_file_name + '.csv', "rb")
-reader = csv.reader(log_file)
+if len(sys.argv) < 2:
+    print "python linux-wifi-aggregator.py log_file_name_without_extension"
+    print "Example: python linux-wifi-aggregator.py logs/mediatek-mt7610u-wifi-rssi-03-10-2017"
+    print 
+else:
+    log_file_name = sys.argv[1]
+    log_file = open(log_file_name + '.csv', "rb")
+    reader = csv.reader(log_file)
 
-agg_file_name = log_file_name + '-agg.csv'
-agg_file = open(agg_file_name, "wb")
-writer = csv.writer(agg_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONE)
+    agg_file_name = log_file_name + '-agg.csv'
+    agg_file = open(agg_file_name, "wb")
+    writer = csv.writer(agg_file, delimiter=',',
+                        quotechar='"', quoting=csv.QUOTE_NONE)
 
-# write headers to aggregated file
-writer.writerow(["nic","antenna","ssid","distance","rssi","link_quality","channel","link_quality_in_percentage"])
+    # write headers to aggregated file
+    writer.writerow(["nic", "antenna", "ssid", "distance", "rssi",
+                     "link_quality", "channel", "link_quality_in_percentage"])
 
-for row in reader:
+    for row in reader:
 
-	# ignore header row
-    if reader.line_num == 1:
-    	pass
-    	
-    # first data row
-    if reader.line_num == 2:
-    	distance = int(row[3])
-    	rssi = int(row[5])
-    	link_quality = int(row[6])
-    	link_quality_in_percentage = int(row[8])
-    	row_count = 1
+            # ignore header row
+        if reader.line_num == 1:
+            pass
 
-    	nic = row[0]
-        antenna = int(row[1])
-        ssid = row[2]
-        channel = int(row[7])
-    	pass
+        # first data row
+        if reader.line_num == 2:
+            distance = int(row[3])
+            rssi = int(row[5])
+            link_quality = int(row[6])
+            link_quality_in_percentage = int(row[8])
+            row_count = 1
 
-    # other data rows
-    if reader.line_num > 2:
-    	if distance == int(row[3]):    		
-    		row_count += 1
-        	rssi += int(row[5])
-        	link_quality += int(row[6])
-        	link_quality_in_percentage += int(row[8])
-        else:        	
-        	rssi_avg = rssi / row_count
-        	link_quality_avg = link_quality / row_count
-        	link_quality_in_percentage_avg = link_quality_in_percentage / row_count
-        	print "Distance : %d, RSSI Avg: %d, Link Avg: %d, Link Percent Avg: %d, Sample Count: %d" % (distance,rssi_avg,link_quality_avg,link_quality_in_percentage_avg,row_count)
-        	writer.writerow([nic,antenna,ssid,distance,rssi_avg,link_quality_avg,channel,link_quality_in_percentage_avg])
-        	
-        	distance = int(row[3])
-	    	rssi = int(row[5])
-	    	link_quality = int(row[6])
-	    	link_quality_in_percentage = int(row[8])
+            nic = row[0]
+            antenna = int(row[1])
+            ssid = row[2]
+            channel = int(row[7])
+            pass
 
-	    	row_count = 1
-        	nic = row[0]
-        	antenna = int(row[1])
-        	ssid = row[2]
-        	channel = int(row[7])
+        # other data rows
+        if reader.line_num > 2:
+            if distance == int(row[3]):
+                row_count += 1
+                rssi += int(row[5])
+                link_quality += int(row[6])
+                link_quality_in_percentage += int(row[8])
+            else:
+                rssi_avg = rssi / row_count
+                link_quality_avg = link_quality / row_count
+                link_quality_in_percentage_avg = link_quality_in_percentage / row_count
+                print "Distance : %d, RSSI Avg: %d, Link Avg: %d, Link Percent Avg: %d, Sample Count: %d" % (distance, rssi_avg, link_quality_avg, link_quality_in_percentage_avg, row_count)
+                writer.writerow([nic, antenna, ssid, distance, rssi_avg,
+                                 link_quality_avg, channel, link_quality_in_percentage_avg])
 
-# save last row
-rssi_avg = rssi / row_count
-link_quality_avg = link_quality / row_count
-link_quality_in_percentage_avg = link_quality_in_percentage / row_count
-print "Distance : %d, RSSI Avg: %d, Link Avg: %d, Link Percent Avg: %d, Sample Count: %d" % (distance,rssi_avg,link_quality_avg,link_quality_in_percentage_avg,row_count)
-writer.writerow([nic,antenna,ssid,distance,rssi_avg,link_quality_avg,channel,link_quality_in_percentage_avg])
-    
-log_file.close()
-agg_file.close()
+                distance = int(row[3])
+                rssi = int(row[5])
+                link_quality = int(row[6])
+                link_quality_in_percentage = int(row[8])
+
+                row_count = 1
+                nic = row[0]
+                antenna = int(row[1])
+                ssid = row[2]
+                channel = int(row[7])
+
+    # save last row
+    rssi_avg = rssi / row_count
+    link_quality_avg = link_quality / row_count
+    link_quality_in_percentage_avg = link_quality_in_percentage / row_count
+    print "Distance : %d, RSSI Avg: %d, Link Avg: %d, Link Percent Avg: %d, Sample Count: %d" % (distance, rssi_avg, link_quality_avg, link_quality_in_percentage_avg, row_count)
+    writer.writerow([nic, antenna, ssid, distance, rssi_avg,
+                     link_quality_avg, channel, link_quality_in_percentage_avg])
+
+    log_file.close()
+    agg_file.close()
